00fd8fb23bb642e2c9af009ba8515ac317481bac
yahiro <yahiro0921@gmail.com>
2019-10-17 20:22:26 +0900

    contentfulを組み込み(うまくいってません)

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,12 @@
+const config = require("~/contentful.json");
+module.exports = {
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+  }
+};
 export default {
   /*
    ** Headers of the page
