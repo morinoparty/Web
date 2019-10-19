047e5fe71775215ea6d6d6e3619fdd5ce0a81e0f
yahirrro <yahiro0921@gmail.com>
2019-10-20 03:29:53 +0900

    restpoint add

--- store/index.js
+++ store/index.js
@@ -1,20 +1,20 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+
+const apiUrl = "https://morino.party";
 
 Vue.use(Vuex);
 
-
-
 export default new Vuex.Store({
-    state: {
-      endpoint: REST_ENDPOINT
-    },
-  
-    mutations: {
-      updateEndpoint(state, endpoint) {
-        state.endpoint = endpoint;
-        bus.$emit("clearError");
-        bus.$emit("bumpViewKey", "Updating content...");
-      }
+  state: {
+    endpoint: `${apiUrl}/wp-json/wp/v2/posts?per_page=100&page=1&_embed=1`
+  },
+
+  mutations: {
+    updateEndpoint(state, endpoint) {
+      state.endpoint = endpoint;
+      bus.$emit("clearError");
+      bus.$emit("bumpViewKey", "Updating content...");
     }
+  }
 });
